9b51e9f38752ca3daba4ca68445b2f71036e7462
Magnus Manske <magnusmanske@googlemail.com>
2019-06-12 10:12:44 +0100

    moved code to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,166 @@
+extern crate mediawiki;
+//#[macro_use]
+extern crate serde_json;
+
+use roxmltree;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub struct Template {
+    pub title: String,
+    pub params: HashMap<String, String>,
+}
+
+impl Template {
+    pub fn new_from_xml(node: &roxmltree::Node) -> Option<Self> {
+        let mut title: Option<String> = None;
+
+        let mut parts: HashMap<String, String> = HashMap::new();
+        for n in node.children().filter(|n| n.is_element()) {
+            if n.tag_name().name() == "title" {
+                n.children().for_each(|c| {
+                    let t = c.text().unwrap_or("").replace("_", " ");
+                    let t = t.trim();
+                    title = Some(t.to_string());
+                });
+            } else if n.tag_name().name() == "part" {
+                let mut k: Option<String> = None;
+                let mut v: Option<String> = None;
+                n.children().for_each(|c| {
+                    let tag = c.tag_name().name();
+                    match tag {
+                        "name" => {
+                            let txt: Vec<String> = c
+                                .children()
+                                .map(|c| c.text().unwrap_or("").trim().to_string())
+                                .collect();
+                            let txt = txt.join("");
+                            if txt.is_empty() {
+                                match c.attribute("index") {
+                                    Some(i) => k = Some(i.to_string()),
+                                    None => {}
+                                }
+                            } else {
+                                k = Some(txt);
+                            }
+                        }
+                        "value" => {
+                            let txt: Vec<String> = c
+                                .children()
+                                .map(|c| c.text().unwrap_or("").trim().to_string())
+                                .collect();
+                            v = Some(txt.join(""));
+                        }
+                        _ => {}
+                    }
+                });
+
+                match (k, v) {
+                    (Some(k), Some(v)) => {
+                        parts.insert(k, v);
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        match title {
+            Some(t) => Some(Self {
+                title: t,
+                params: parts,
+            }),
+            None => None,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ListeriaPage {
+    mw_api: mediawiki::api::Api,
+    page: String,
+    template_title_start: String,
+    language: String,
+    template: Option<Template>,
+}
+
+impl ListeriaPage {
+    pub fn new(mw_api: &mediawiki::api::Api, page: String) -> Option<Self> {
+        let mut ret = Self {
+            mw_api: mw_api.clone(),
+            page: page,
+            template_title_start: "Wikidata list".to_string(),
+            language: mw_api.get_site_info_string("general", "lang").ok()?,
+            template: None,
+        };
+        ret.load_page().ok();
+        Some(ret)
+    }
+
+    pub fn load_page(self: &mut Self) -> Result<(), String> {
+        let params: HashMap<String, String> = vec![
+            ("action", "parse"),
+            ("prop", "parsetree"),
+            ("page", self.page.as_str()),
+        ]
+        .iter()
+        .map(|x| (x.0.to_string(), x.1.to_string()))
+        .collect();
+
+        let result = self
+            .mw_api
+            .get_query_api_json(&params)
+            .expect("Loading page failed");
+        let doc = match result["parse"]["parsetree"]["*"].as_str() {
+            Some(text) => roxmltree::Document::parse(&text).unwrap(),
+            None => return Err(format!("No parse tree for {}", &self.page)),
+        };
+        doc.root()
+            .descendants()
+            .filter(|n| n.is_element() && n.tag_name().name() == "template")
+            .for_each(|node| {
+                if self.template.is_some() {
+                    return;
+                }
+                match Template::new_from_xml(&node) {
+                    Some(t) => {
+                        if t.title == self.template_title_start {
+                            self.template = Some(t);
+                        }
+                    }
+                    None => {}
+                }
+            });
+        match &self.template {
+            Some(_) => Ok(()),
+            None => Err(format!(
+                "No template '{}' found",
+                &self.template_title_start
+            )),
+        }
+    }
+
+    pub fn run_query(self: &mut Self) -> Result<(), String> {
+        let t = match &self.template {
+            Some(t) => t,
+            None => return Err(format!("No template found")),
+        };
+        let sparql = match t.params.get("sparql") {
+            Some(s) => s,
+            None => return Err(format!("No `sparql` parameter in {:?}", &t)),
+        };
+
+        let wd_api = mediawiki::api::Api::new("https://www.wikidata.org/w/api.php")
+            .expect("Could not connect to Wikidata API");
+        println!("Running SPARQL: {}", &sparql);
+        let _j = match wd_api.sparql_query(sparql) {
+            Ok(j) => j,
+            Err(e) => return Err(format!("{:?}", &e)),
+        };
+        //println!("{}", ::serde_json::to_string_pretty(&j).unwrap());
+        Ok(())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
