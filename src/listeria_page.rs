70675ee80570316bbc6e16deca96c104a11d867c
Magnus Manske <mm6@sanger.ac.uk>
2021-05-20 08:56:07 +0100

    fix for #72

--- src/listeria_page.rs
+++ src/listeria_page.rs
@@ -535,6 +535,11 @@ mod tests {
     }
 
     #[tokio::test]
+    async fn image_pipe() {
+        check_fixture_file(PathBuf::from("test_data/image_pipe.fixture")).await;
+    }
+
+    #[tokio::test]
     async fn edit_wikitext() {
         let data = read_fixture_from_file(PathBuf::from("test_data/edit_wikitext.fixture"));
         let mw_api = wikibase::mediawiki::api::Api::new("https://en.wikipedia.org/w/api.php")
