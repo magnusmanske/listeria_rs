a85209c84e464e0e88577b60d0684f4e8acb4bfc
Magnus Manske <mm6@sanger.ac.uk>
2023-02-01 08:55:23 +0000

    threads as argv[1]

--- src/bin/bot.rs
+++ src/bin/bot.rs
@@ -5,6 +5,7 @@ use std::{sync::Arc, convert::TryInto};
 use tokio::{sync::Mutex, runtime};
 use listeria::listeria_bot::ListeriaBot;
 use tokio::time::{sleep, Duration};
+use std::env;
 
 /*
 TEST DB CONNECT
@@ -19,15 +20,15 @@ cd ~/listeria_rs ; jsub -mem 6g -cwd -continuous ./target/release/bot
 # TODO freq
 */
 
-const THREADS: usize = 4;
+const DEFAULT_THREADS: usize = 4;
 
-async fn run_singles() {
+async fn run_singles(threads: usize) {
     let running_counter = Arc::new(Mutex::new(0 as i32));
     let bot = ListeriaBot::new("config.json").await.unwrap();
     let _ = bot.reset_running().await;
     let bot = Arc::new(bot);
     loop {
-        while *running_counter.lock().await>=THREADS.try_into().expect("Can't convert THREADS to usize") {
+        while *running_counter.lock().await>=threads.try_into().expect("Can't convert threads to usize") {
             sleep(Duration::from_millis(100)).await;
         }
         let page = match bot.prepare_next_single_page().await {
@@ -49,16 +50,21 @@ async fn run_singles() {
 
 //#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let argv: Vec<_> = env::args_os().collect();
+    let threads = match argv.get(1) {
+        Some(t) => t.to_owned().into_string().unwrap_or("".into()).parse::<usize>().unwrap_or(DEFAULT_THREADS),
+        None => DEFAULT_THREADS
+    };
     
     let threaded_rt = runtime::Builder::new_multi_thread()
         .enable_all()
-        .worker_threads(THREADS)
+        .worker_threads(threads)
         .thread_name("listeria")
-        .thread_stack_size(THREADS * 1024 * 1024)
+        .thread_stack_size(threads * 1024 * 1024)
         .build()?;
 
     threaded_rt.block_on(async move {
-        run_singles().await;
+        run_singles(threads).await;
     });
     Ok(())
 }
