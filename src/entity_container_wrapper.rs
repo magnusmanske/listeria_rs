de73e4e6550167d2d8c44edf6cfd9913745cbd60
Magnus Manske <magnusmanske@googlemail.com>
2024-03-11 11:46:14 +0000

    entity cache fix

--- src/entity_container_wrapper.rs
+++ src/entity_container_wrapper.rs
@@ -171,7 +171,7 @@ impl EntityContainerWrapper {
                 let data_value = s.main_snak().data_value().to_owned()?;
                 match data_value.value() {
                     wikibase::Value::StringValue(s) => {
-                        Some(s.to_owned().replace("$1", &urlencoding::decode(&id).ok()?))
+                        Some(s.to_owned().replace("$1", &urlencoding::decode(id).ok()?))
                     }
                     _ => None,
                 }
@@ -181,6 +181,7 @@ impl EntityContainerWrapper {
 
     pub fn get_datatype_for_property(&self, prop: &str) -> SnakDataType {
         match self.get_entity(prop) {
+            /* trunk-ignore(clippy/collapsible_match) */
             Some(entity) => match entity {
                 Entity::Property(p) => match p.datatype() {
                     Some(t) => t.to_owned(),
@@ -192,10 +193,7 @@ impl EntityContainerWrapper {
         }
     }
 
-    pub fn gather_entities_and_external_properties(
-        &self,
-        parts: &[PartWithReference],
-    ) -> Vec<String> {
+    pub fn gather_entities_and_external_properties(parts: &[PartWithReference]) -> Vec<String> {
         let mut entities_to_load = vec![];
         for part_with_reference in parts {
             match &part_with_reference.part {
@@ -205,8 +203,7 @@ impl EntityContainerWrapper {
                 ResultCellPart::ExternalId((property, _id)) => {
                     entities_to_load.push(property.to_owned());
                 }
-                ResultCellPart::SnakList(v) => self
-                    .gather_entities_and_external_properties(v)
+                ResultCellPart::SnakList(v) => Self::gather_entities_and_external_properties(v)
                     .iter()
                     .for_each(|entity_id| entities_to_load.push(entity_id.to_string())),
                 _ => {}
