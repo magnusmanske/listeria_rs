eec2b64136b2b9726cbbe2dbfa0a48de8b047c45
Magnus Manske <magnusmanske@googlemail.com>
2024-03-11 10:02:27 +0000

    self-termination if no bew jobs start

--- src/wiki_page_result.rs
+++ src/wiki_page_result.rs
@@ -21,4 +21,41 @@ impl WikiPageResult {
     pub fn fail(wiki: &str, page: &str, message: &str) -> Self {
         Self::new(wiki, page, "FAIL", message.to_string())
     }
+
+    pub fn standardize_meassage(&mut self) {
+        if self
+            .message
+            .contains("This page is a translation of the page")
+        {
+            self.result = "TRANSLATION".into();
+            self.message = "This page is a translation".into();
+        }
+        if self
+            .message
+            .contains("Connection reset by peer (os error 104)")
+        {
+            self.message = "104_RESET_BY_PEER".into();
+        }
+        if self.message.contains("api.php): operation timed out") {
+            self.message = "WIKI_TIMEOUT".into();
+        }
+        if self.message.contains("/sparql): operation timed out") {
+            self.message = "SPARQL_TIMEOUT".into();
+        }
+        if self
+            .message
+            .contains("expected value at line 1 column 1: SPARQL-QUERY:")
+        {
+            self.message = "SPARQL_ERROR".into();
+        }
+        if self.message.contains("No 'sparql' parameter in Template") {
+            self.message = format!("SPARQL_ERROR {}", self.message);
+        }
+        if self
+            .message
+            .contains("Could not determine SPARQL variable for item")
+        {
+            self.message = format!("SPARQL_ERROR {}", self.message);
+        }
+    }
 }
