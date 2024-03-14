b175c753ad2474cb53ef4faf9f37ec48de91ef02
Magnus Manske <magnusmanske@googlemail.com>
2024-03-14 13:09:21 +0000

    test fixes and refactoring

--- src/page_element.rs
+++ src/page_element.rs
@@ -21,44 +21,13 @@ pub struct PageElement {
 
 impl PageElement {
     pub fn new_from_text(text: &str, page: &ListeriaPage) -> Option<Self> {
-        let start_template = page
-            .config()
-            .get_local_template_title_start(page.wiki())
-            .ok()?;
-        let end_template = page
-            .config()
-            .get_local_template_title_end(page.wiki())
-            .ok()?;
-        let pattern_string_start = r#"\{\{(Wikidata[ _]list[^\|]*|"#.to_string()
-            + &start_template.replace(' ', "[ _]")
-            //+ r#")\s*\|"#; // New version
-            + r#"[^\|]*)"#; // Orig
-                            //+ r#")"#;
-        let pattern_string_end = r#"\{\{(Wikidata[ _]list[ _]end|"#.to_string()
-            + &end_template.replace(' ', "[ _]")
-            + r#")(\s*\}\})"#;
-        let seperator_start: Regex = RegexBuilder::new(&pattern_string_start)
-            .multi_line(true)
-            .dot_matches_new_line(true)
-            .case_insensitive(true)
-            .build()
-            .ok()?;
-        let seperator_end: Regex = RegexBuilder::new(&pattern_string_end)
-            .multi_line(true)
-            .dot_matches_new_line(true)
-            .case_insensitive(true)
-            .build()
-            .ok()?;
+        let (seperator_start, seperator_end) = Self::get_start_stop_separators(page)?;
 
-        let match_start = match seperator_start.find(text) {
-            Some(m) => m,
-            None => return None,
-        };
-
-        let (match_end, single_template) = match seperator_end.find_at(text, match_start.start()) {
-            Some(m) => (m, false),
-            None => (match_start, true), // No end template, could be tabbed data
-        };
+        let (match_start, match_end, single_template) =
+            match Self::matches_from_separators(seperator_start, text, seperator_end) {
+                Ok(value) => value,
+                Err(value) => return value,
+            };
 
         let remaining = if single_template {
             String::from_utf8(text.as_bytes()[match_start.end()..].to_vec()).ok()?
@@ -181,5 +150,52 @@ impl PageElement {
         } else {
             None
         }
+    }
+
+    fn get_start_stop_separators(page: &ListeriaPage) -> Option<(Regex, Regex)> {
+        let start_template = page
+            .config()
+            .get_local_template_title_start(page.wiki())
+            .ok()?;
+        let end_template = page
+            .config()
+            .get_local_template_title_end(page.wiki())
+            .ok()?;
+        let pattern_string_start = r#"\{\{(Wikidata[ _]list[^\|]*|"#.to_string()
+        + &start_template.replace(' ', "[ _]")
+        //+ r#")\s*\|"#; // New version
+        + r#"[^\|]*)"#;
+        let pattern_string_end = r#"\{\{(Wikidata[ _]list[ _]end|"#.to_string()
+            + &end_template.replace(' ', "[ _]")
+            + r#")(\s*\}\})"#;
+        let seperator_start: Regex = RegexBuilder::new(&pattern_string_start)
+            .multi_line(true)
+            .dot_matches_new_line(true)
+            .case_insensitive(true)
+            .build()
+            .ok()?;
+        let seperator_end: Regex = RegexBuilder::new(&pattern_string_end)
+            .multi_line(true)
+            .dot_matches_new_line(true)
+            .case_insensitive(true)
+            .build()
+            .ok()?;
+        Some((seperator_start, seperator_end))
+    }
+
+    fn matches_from_separators(
+        seperator_start: Regex,
+        text: &str,
+        seperator_end: Regex,
+    ) -> Result<(regex::Match<'_>, regex::Match<'_>, bool), Option<PageElement>> {
+        let match_start = match seperator_start.find(text) {
+            Some(m) => m,
+            None => return Err(None),
+        };
+        let (match_end, single_template) = match seperator_end.find_at(text, match_start.start()) {
+            Some(m) => (m, false),
+            None => (match_start, true), // No end template, could be tabbed data
+        };
+        Ok((match_start, match_end, single_template))
     }
 }
