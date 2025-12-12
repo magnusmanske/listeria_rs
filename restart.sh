3160949ac9f0c854f9e4f9f629f7805dd1c79c63
Magnus Manske <magnusmanske@googlemail.com>
2025-12-12 10:42:35 +0200

    merging into single binary

--- restart.sh
+++ restart.sh
@@ -2,5 +2,5 @@
 toolforge jobs delete rustbot
 toolforge jobs run --mem 5000Mi --cpu 3 --continuous --mount=all \
 --image tool-listeria/tool-listeria:latest \
---command "sh -c 'target/release/bot /data/project/listeria/listeria_rs/config.json'" \
+--command "sh -c 'target/release/main wikidata --config /data/project/listeria/listeria_rs/config.json'" \
 rustbot
