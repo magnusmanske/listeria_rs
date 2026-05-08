bd1470e5b112d3c3266a110b2ff2260d6c2c5e47
Magnus Manske <magnusmanske@googlemail.com>
2026-05-08 09:46:25 +0100

    misc

--- restart.sh
+++ restart.sh
@@ -2,5 +2,8 @@
 toolforge jobs delete rustbot
 toolforge jobs run --mem 5000Mi --cpu 3 --continuous --mount=all \
 --image tool-listeria/tool-listeria:latest \
---command "sh -c 'target/release/main --config /data/project/listeria/listeria_rs/config.json wikidata'" \
+--command "target/release/main --config /data/project/listeria/listeria_rs/config.json wikidata" \
 rustbot
+
+
+# --command "sh -c 'target/release/main --config /data/project/listeria/listeria_rs/config.json wikidata'" \
